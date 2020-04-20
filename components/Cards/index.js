9aad9b663088dc0a1bdc64628f2779af4177cf30
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-04-19 19:43:27 -0500

    On step 3

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,45 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+function cardsMaker(headlineData, authorData){
+    let card = document.createElement('div')
+    card.classList.add('card')
+
+    let headline = document.createElement('div')
+    headline.classList.add('headline')
+    headline.textContent = headlineData;
+
+    let author = document.createElement('div')
+    author.classList.add('author')
+
+    let cardImg = document.createElement('div')
+    cardImg.classList.add('img-container')
+    cardImg.setAttribute('src', ``)
+
+    let authorSpan = document.createElement('span')
+    authorSpan.textContent = authorData;
+
+    card.appendChild(headline)
+    headline.appendChild(author)
+    author.appendChild(cardImg)
+    author.appendChild(authorSpan)
+
+    return card;
+
+}
+
+cardsMaker();
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,34 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+
+function tabMaker(topicData){
+    let tabs = document.createElement('div')
+    tabs.classList.add('tab')
+    tabs.textContent = topicData;
+
+    return tabs;
+}
+
+let tabTopics = document.querySelector('.topics')
+
+let topicArray = ['javascript', 'bootstrap', 'technology', 'jquery', 'node.js']
+
+topicArray.forEach ((item) => {
+    let tabs = tabMaker(item);
+    tabTopics.appendChild(tabs);
+});
