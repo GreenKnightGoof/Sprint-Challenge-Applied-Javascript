b332daeb253a7ca1ff08f1002689b7fb7979836e
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-06-12 19:06:59 -0500

    Cards are done, starting code clean up

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,57 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+let cardsElement = document.querySelector(".cards-container");
+
+axios
+  .get(`https://lambda-times-backend.herokuapp.com/articles`)
+  .then(function (response) {
+    const articleCards = response.data.articles;
+    for (const property in articleCards) {
+      articleCards[property].map((element) => {
+        const cardsComponent = cardsCreator(
+          element.headline,
+          element.authorPhoto,
+          element.authorName,
+          element.cardImg,
+          element.authorSpan
+        );
+        cardsElement.appendChild(cardsComponent);
+      });
+    }
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+function cardsCreator(headlineData, authorPhoto, authorName) {
+  let cards = document.createElement("div");
+  cards.classList.add("card");
+
+  let headlineElement = document.createElement("div");
+  headlineElement.classList.add("headline");
+  headlineElement.textContent = headlineData;
+
+  let authorElement = document.createElement("div");
+  authorElement.classList.add("author");
+
+  let cardImgElement = document.createElement("div");
+  cardImgElement.classList.add("img-container");
+
+  let cardImg = document.createElement("img");
+  cardImg.src = authorPhoto;
+
+  let authorSpan = document.createElement("span");
+  authorSpan.textContent = authorName;
+
+  cards.appendChild(headlineElement);
+  cards.appendChild(authorElement);
+  authorElement.appendChild(cardImgElement);
+  authorElement.appendChild(authorSpan);
+  cardImgElement.appendChild(cardImg);
+  return cards;
+}
+
+cardsCreator();
